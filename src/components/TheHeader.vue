224d229f0205f5b4ebe766d3463ee72b716ab1ec
Wadson Garbes <nenogarbes@hotmail.com>
2021-10-14 17:18:01 -0300

    Cria TheHeader e TheFooter

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,57 @@
+<template>
+<header class="header">
+    <div class="container">
+      <router-link to="/cursos" class="grid-4">
+        <img src="@/assets/logo.svg" alt="Logotipo Esquadrilife" />
+        </router-link>
+      <nav class="grid-12 header-menu">
+        <ul>
+          <li><router-link to="/sobre">Sobre</router-link></li>
+          <li><router-link to="/obras">Obras</router-link></li>
+          <li><router-link to="/produtos">Produtos</router-link></li>
+          <li><router-link to="/contato">Contato</router-link></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  name: "TheHeader",
+};
+</script>
+<style scoped>
+/* Header */
+.header {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background: #a65823;
+  padding: 30px 0;
+  z-index: 1;
+}
+
+.header-menu {
+  text-align: right;
+}
+
+.header-menu ul li {
+  margin-top: 30px;
+  display: inline-block;
+  margin-left: 20px;
+}
+
+.header-menu ul li a {
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+  font-size: 14px;
+  line-height: 20px;
+  padding: 10px 0;
+}
+
+.header-menu ul li a:hover {
+  color: #82f7ff;
+}
+</style>

ff69020656b8160a22f8a6347e5024f788122dd8
wadsongarbes <nenogarbes@hotmail.com>
2021-10-19 16:24:35 -0300

    Adiciona componente TheConstruction

---
+++ src/components/TheConstruction.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="construction-item">
+    <img :src="imgUrl" alt="Obra 1" />
+    <div>
+      <h2>{{ title }}</h2>
+      <p>{{ description }}</p>
+      <a :href="constructionUrl">Ver Obra</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "TheConstruction",
+  props: ["imgUrl", "title", "description", "constructionUrl"],
+};
+</script>
+<style>
+.construction-item {
+  clear: both;
+  margin: 20px auto;
+}
+
+.construction-item::after,
+.construction-item::before {
+  display: table;
+  content: "";
+  clear: both;
+}
+
+.construction-item img {
+  width: 460px;
+  float: left;
+  display: block;
+  margin: 0 10px;
+}
+
+.construction-item > div {
+  width: 460px;
+  float: left;
+  display: block;
+  margin: 0 10px;
+}
+
+.construction-item h2 {
+  font-size: 2.25em;
+  margin-bottom: 20px;
+  color: #a65823;
+}
+
+.construction-item > p {
+  font-size: 1.125em;
+  line-height: 1.4em;
+  max-width: 320px;
+  margin-bottom: 40px;
+}
+
+.construction-item a {
+  font-size: 1.125em;
+  text-transform: uppercase;
+  text-decoration: none;
+  border: 3px solid;
+  color: #a65823;
+  padding: 10px 20px;
+}
+
+.construction-item a:hover {
+  color: #82f7ff;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,51 @@
 <template>
-  <TheIntroduction
-    h1Content="Esquadrias Especiais"
-    blockquoteContent='"Não tenha nada em sua casa que você não considere útil ou acredita ser bonito"'
-    citeContent="William Morris"
-    :imgUrl="require('@/assets/constructions/intro.png')"
-  ></TheIntroduction>
+  <div>
+    <TheIntroduction
+      h1Content="Esquadrias Especiais"
+      blockquoteContent='"Não tenha nada em sua casa que você não considere útil ou acredita ser bonito"'
+      citeContent="William Morris"
+      :imgUrl="require('@/assets/constructions/intro.png')"
+    ></TheIntroduction>
+    <section class="construction">
+      <TheConstruction
+        :imgUrl="require('@/assets/constructions/banner01.png')"
+        title="Obra Morumbi"
+        description="Linha utilizada: Dinaflex"
+        constructionUrl="/obras/1"
+      ></TheConstruction>
+      <TheConstruction
+        :imgUrl="require('@/assets/constructions/banner02.png')"
+        title="Obra Jardim Europa"
+        description="Linha utilizada: Euroconcept"
+        constructionUrl="/obras/2"
+      ></TheConstruction>
+      <TheConstruction
+        :imgUrl="require('@/assets/constructions/banner03.png')"
+        title="Obra Pinheiros"
+        description="Linha utilizada: Wallglass"
+        constructionUrl="/obras/3"
+      ></TheConstruction>
+    </section>
+  </div>
 </template>
 <script>
 import TheIntroduction from "@/components/TheIntroduction.vue";
+import TheConstruction from "@/components/TheConstruction.vue";
 
 export default {
   name: "home",
   components: {
     TheIntroduction,
+    TheConstruction,
   },
 };
 </script>
-<style></style>
+<style>
+/* Construction section */
+
+.construction {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 60px 0;
+}
+</style>

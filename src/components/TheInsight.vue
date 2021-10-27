cc626aeac91cd89b327bf8e9264ac06a43bd7867
Wadson Garbes <nenogarbes@hotmail.com>
2021-10-27 15:15:59 -0300

    Finaliza a página obras

---
+++ src/components/TheInsight.vue
@@ -0,0 +1,87 @@
+<template>
+  <section
+    class="obra"
+    :style="{ 'background-image': 'url(' + imgUrl + ')' }"
+  >
+    <div class="obras-item">
+        <h2>{{ Construction }}</h2>
+        <p>{{ Line }}</p>
+        <a href="#">Ver Obra</a>
+      </div>
+  </section>
+</template>
+<script>
+export default {
+  name: "TheIntroduction",
+  props: ["Construction", "Line", "imgUrl"],
+};
+</script>
+<style>
+/* Obras */
+.obra {
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 100%;
+  height: 380px;
+  padding-top: 120px;
+}
+
+.obras-item {
+  clear: both;
+  text-align: center;
+  max-width: 460px;
+  height: 200px;
+  margin: 0 auto;
+  color: #a65823;
+  background: rgba(255, 255, 255, 0.6);
+}
+
+.obras-item p {
+  text-transform: uppercase;
+  text-align: center;
+  clear: both;
+  margin: 40px auto;
+}
+
+.obras-item::after,
+.obras-item::before {
+  display: table;
+  content: "";
+  clear: both;
+}
+
+.obras-item > div {
+  width: 460px;
+  float: left;
+  display: block;
+  margin: 0 10px;
+}
+
+.obras-item h2 {
+  text-transform: uppercase;
+  font-size: 2.25em;
+  margin-bottom: 20px;
+  color: #a65823;
+}
+
+.obras-item p {
+  font-size: 1.125em;
+  line-height: 1.4em;
+  max-width: 320px;
+  margin-bottom: 40px;
+}
+
+.obras-item a {
+  font-size: 1.125em;
+  text-transform: uppercase;
+  text-decoration: none;
+  border: 3px solid;
+  color: #a65823;
+  padding: 10px 20px;
+}
+
+.obras-item a:hover {
+  color: #82f7ff;
+}
+</style>
---
+++ src/views/Constructions.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <TheIntroduction
+        h1Content="Obras"
+        blockquoteContent='"Não tenha nada em sua casa que você não considere útil ou acredita ser bonito"'
+        citeContent="William Morris"
+        :imgUrl="require('@/assets/constructions/intro.png')"
+        ></TheIntroduction>
+        <TheInsight
+            Construction="Obra Jauaperi"
+            Line="Wallglass"
+            :imgUrl="require('@/assets/constructions/intro.png')">
+        </TheInsight>
+        <TheInsight
+            Construction="Obra Jauaperi"
+            Line="Wallglass"
+            :imgUrl="require('@/assets/constructions/intro.png')">
+        </TheInsight>
+        <TheInsight
+            Construction="Obra Jauaperi"
+            Line="Wallglass"
+            :imgUrl="require('@/assets/constructions/intro.png')">
+        </TheInsight>
+        <TheInsight
+            Construction="Obra Jauaperi"
+            Line="Wallglass"
+            :imgUrl="require('@/assets/constructions/intro.png')">
+        </TheInsight>
+    </div>
+</template>
+<script>
+import TheIntroduction from "@/components/TheIntroduction.vue";
+import TheInsight from "@/components/TheInsight.vue";
+
+export default {
+    name: "construction",
+    components: {
+        TheIntroduction,
+        TheInsight
+    }
+
+    
+}
+</script>
+
+<style>
+
+</style>
